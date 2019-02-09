fa4b9f157967fb70c0093008f6de8bed2bd02e5d
Gislene Vanessa <vann.n.a0044@gmail.com>
2019-02-09 19:30:48 -0300

    criação de models e admin

--- atendimento/admin.py
+++ atendimento/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Funcionario, Paciente, Equipe
 
 # Register your models here.
+
+
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'cpf', 'data_nasc', 'idade']
+    search_fields = ['nome', 'cpf']
+
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'cpf', 'matricula', 'quarto', 'data_nasc', 'idade']
+    search_fields = ['nome', 'cpf', 'matricula']
+
+class EquipeAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao']
+
+
+
+admin.site.register(Funcionario, FuncionarioAdmin)
+admin.site.register(Paciente, PacienteAdmin)
+admin.site.register(Equipe, EquipeAdmin)
+
--- atendimento/models.py
+++ atendimento/models.py
@@ -2,15 +2,49 @@ from django.db import models
 
 # Create your models here.
 
-# class Pessoa(models.Model):
-#     nome = models.Charfiel(max_length=200, verbose_name='Nome', null=False, blank=False)
-#     cpf = models.IntegerField(verbose_name='CPF', null=False, blank=False)
-#     data_nasc = models.DateField(verbose_name='Data de Nascimento', null=False, blank=False)
-#     idade = models.IntegerField(verbose_name='Idade', null=False, blank=False)
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=128, verbose_name='Nome', null=False, blank=False)
+    cpf = models.IntegerField(verbose_name='CPF', null=False, blank=False)
+    data_nasc = models.DateField(verbose_name='Data de Nascimento', null=False, blank=False)
+    idade = models.IntegerField(verbose_name='Idade', null=False, blank=False)
 
-#     class Meta: 
-#         abstract=True
+    class Meta: 
+        abstract=True
     
-# class Funcionario(Pessoa):
-#     matricula = models.Charfiel(max_length=200, verbose_name='Nome', null=False, blank=False)
+
+class Equipe(models.Model):
+    nome = models.CharField(max_length=12, verbose_name='Nome', null=False, blank=False)
+    descricao = models.CharField(max_length=128,verbose_name='Descrição da Atividade', null=False, blank=False)
     
+    class Meta: 
+        verbose_name = 'Equipe'
+        verbose_name_plural = 'Equipes'
+    
+    def __str__(self):
+        return '%s' % (self.nome) 
+
+
+class Funcionario(Pessoa):
+    matricula = models.CharField(max_length=12, verbose_name='Matricula', null=False, blank=False)
+    função = models.CharField(max_length=128, verbose_name='Função', null=False, blank=False)
+    equipe = models.ForeignKey(Equipe, on_delete=models.CASCADE)
+
+    class Meta: 
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+    
+    def __str__(self):
+        return '%s' % (self.nome) 
+
+
+class Paciente(Pessoa):
+
+    matricula = models.CharField(max_length=12, verbose_name='Matricula', null=False, blank=False)
+    quarto = models.CharField(max_length=128,verbose_name='Quarto', null=False, blank=False)
+    
+    class Meta: 
+        verbose_name = 'Paciente'
+        verbose_name_plural = 'Pacientes'
+    
+    def __str__(self):
+        return '%s' % (self.nome) 
