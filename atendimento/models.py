1c814891e5d08f2900bb4f0bbfaf08c2bcc5af1b
Gislene Vanessa <gislene.silva@aguasdeteresina.com.br>
2019-02-09 18:45:18 -0300

    inicial

---
+++ atendimento/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+# class Pessoa(models.Model):
+#     nome = models.Charfiel(max_length=200, verbose_name='Nome', null=False, blank=False)
+#     cpf = models.IntegerField(verbose_name='CPF', null=False, blank=False)
+#     data_nasc = models.DateField(verbose_name='Data de Nascimento', null=False, blank=False)
+#     idade = models.IntegerField(verbose_name='Idade', null=False, blank=False)
+
+#     class Meta: 
+#         abstract=True
+    
+# class Funcionario(Pessoa):
+#     matricula = models.Charfiel(max_length=200, verbose_name='Nome', null=False, blank=False)
+    
